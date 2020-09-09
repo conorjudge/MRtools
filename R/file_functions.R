4e3739d8ecdb81005ccddce91db1490696765e8e
Conor Judge <conorjudge@gmail.com>
2020-09-09 00:57:45 -0400

    Add all CKDGen datasets to MRtools

--- R/file_functions.R
+++ R/file_functions.R
@@ -1,13 +1,13 @@
 
 
-extract_significant_SNPs <- function(filename_in,pval_exposure_name,pval_exposure_name_and_cutoff,filename_out) {
+extract_significant_SNPs <- function(filename_in,pval_exposure_name,filename_out) {
 
-  print(paste0("Extract SNP/Exposure associations with p<", p_cutoff))
+  print(paste0("Extract SNP/Exposure associations with p<0.00000005"))
   print(paste0("From: ", filename_in))
 
   if (file.exists(filename_in)){
 
-    exposure_dat_dt <- data.table::fread(filename_in)
+    exposure_dat_dt <- data.table::fread(filename_in,fill=TRUE)
 
     data.table::setnames(exposure_dat_dt, paste0(pval_exposure_name), "pvalue")
     data.table::setkey(exposure_dat_dt, pvalue)
@@ -18,8 +18,7 @@ extract_significant_SNPs <- function(filename_in,pval_exposure_name,pval_exposur
 
     print(nrow(exposure_dat_dt_significant))
 
-    return(exposure_dat_dt_significant)
-
+    data.table::fwrite(exposure_dat_dt_significant,filename_out)
   }
 
 }
@@ -35,4 +34,26 @@ more_file <- function(filename_in,num_rows) {
 }
 
 #more_file("./data-raw/20171016_MW_eGFR_overall_ALL_nstud61.dbgap.txt",1)
+
+read_exposure_data_CKDGen <- function(filename_in) {
+  print(filename_in)
+
+  filename_in2=deparse(substitute(filename_in))
+
+  #Map column names from CKDGEN file to TwoSampleMR spec
+  exposure_dat <- TwoSampleMR::read_exposure_data(
+    filename = filename_in2,
+    sep = ",",
+    snp_col = "RSID",
+    beta_col = "Effect",
+    se_col = "StdErr",
+    effect_allele_col = "Allele1",
+    other_allele_col = "Allele2",
+    eaf_col = "Freq1",
+    pval_col = "pvalue",
+    samplesize_col = "n_total_sum"
+  )
+
+  return(exposure_dat)
+}
 
